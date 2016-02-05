68ea9c95cd22f88c0b0a963eaaa2b0095f55a06c
Nick Gamble <nicobottema@hotmail.com>
2016-02-05 21:36:17 +0100

    Changes

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -1,3 +1,5 @@
+var apiHost = "127.0.0.1";
+var apiPort = 3000;
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -32,14 +34,33 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
-
+  .state('tab.login', {
+    url: '/login',
+    views: {
+      'tab.login': {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+  
+  .state('tab.register', {
+    url: '/register',
+    views: {
+      'tab.register': {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterCtrl'
+      }
+    }
+  })
+  
   .state('tab.ww', {
     url: '/ww',
     views: {
@@ -50,6 +71,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
+
+  
   .state('tab.alt', {
       url: '/alt',
       views: {
@@ -83,3 +106,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $urlRouterProvider.otherwise('/tab/ww');
 
 });
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -2,6 +2,53 @@ var test =  "test";
 
 angular.module('starter.controllers', [])
 
+.controller('LoginCtrl', function($scope) {
+	$scope.credentials = {
+		username: "",
+		password: ""
+	}
+	
+	$scope.login = function() {
+		alert($scope.credentials.username);
+	}
+})
+
+.controller('RegisterCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.credentials = {
+		email: "",
+		username: "",
+		password1: "",
+		password2: ""
+	}
+		
+	$scope.register = function() {
+		if(!isValidEmailAddress($scope.credentials.email)) {
+			alert("Please enter a valid email address");
+		} else if($scope.credentials.username.length < 4) {
+			alert("Please enter a username of at least 4 characters");
+		} else if($scope.credentials.password1.length < 4) {
+			alert("Please enter a password of at least 4 characters");
+		} else if($scope.credentials.password1 != $scope.credentials.password2)	{
+			alert("Please make sure the passwords match");
+		} else {
+			$http({
+				method: 'POST',
+				url: "http://" + apiHost + ":" + apiPort + "/users",
+				data: { 
+					username: $scope.credentials.username,
+					display_name: $scope.credentials.username,
+					email: $scope.credentials.email,
+					password: $scope.credentials.password1
+				}
+			}).then(function successCallback(response) {
+				alert(response.data.toSource());
+			}, function errorCallback(response) {
+				console.log(response);
+			});
+		}
+	}
+}])
+
 .controller('WwCtrl', function($scope) {})
 
 .controller('AltCtrl', function($scope) {})
